4746bf56bdb805fa5697ade42fd26f492f88513a
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-02-23 15:24:21 +0100

    add gsam and animate first section

--- src/components/sections/Mainsection.js
+++ src/components/sections/Mainsection.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 import Donuts from '../../images/donuts-main-baner.jpg';
 
 import '../../styles/Mainsection.scss';
 
 const Mainsection = () => {
+   gsap.registerPlugin(ScrollTrigger);
+
+   const donutsBg = useRef(null);
+
+   useEffect(
+      () => {
+         const tl = gsap.timeline({ScrollTrigger: {trigger: '.mainSection', scrub: true, start: 'top top', end: 'bottom top' }});
+         tl.to(donutsBg.current, {y: '-=100'})
+      },
+      []
+   )
+
    return (
       <section className="mainSection">
          <h1 className="mainSection__title">Donuts.</h1>
@@ -12,6 +26,7 @@ const Mainsection = () => {
          src={Donuts} 
          alt="Obrazek w tle przedstawiający donuty" 
          className="mainSection__background"
+         ref={donutsBg}
          />
       </section>
    );
