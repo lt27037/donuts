b9006b527021b6b1f96d8129177effb44b621738
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-02-23 13:48:02 +0100

    initial commit

---
+++ src/components/Mainsection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Mainsection.scss';
+
+const Mainsection = () => {
+   return (
+      <section className="mainSection">
+
+      </section>
+   );
+};
+
+export default Mainsection;
