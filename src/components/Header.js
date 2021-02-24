c31714a4bae26e1c5125d91954464481f4603c7b
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-02-24 09:46:57 +0100

    add header component and logo svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 import Logo from '../images/Logo-mini.png';
+import { ReactComponent as LogoSvg } from '../images/Logo-mini.svg';
 
 import '../styles/Header.scss';
 
 const Header = () => {
+   gsap.registerPlugin(ScrollTrigger);
+   const logoElement = useRef(null);
+   
+
+   useEffect(
+      () => {
+
+         const tl = gsap.timeline({
+            scrollTrigger: {
+               trigger: '.topProducts',
+               start: 'top top',
+            }
+         });
+
+         tl.add(() => {
+            let stroke = logoElement.current.getElementsByClassName('logoPart');
+            let fill = logoElement.current.getElementsByClassName('arrow');
+            stroke.forEach(element => element.style.stroke = "#000");
+            fill.forEach(element => element.style.fill = "#000");
+         })
+         console.log(logoElement.current);
+
+         let wrapper = logoElement.current;
+
+         let stroke = logoElement.current.getElementsByClassName('logoPart');
+         let fill = logoElement.current.getElementsByClassName('arrow');
+         
+      },
+      []
+   )
+
+
    return (
       <header className="mainHeader">
-         <img src={Logo} alt="Logo sklepu" className="mainHeader__logo"/>
+         <LogoSvg 
+         className="mainHeader__logo" 
+         ref={logoElement}
+         />
       </header>
    );
 };
--- src/components/sections/Mainsection.js
+++ src/components/sections/Mainsection.js
@@ -7,14 +7,24 @@ import Donuts from '../../images/donuts-main-baner.jpg';
 import '../../styles/Mainsection.scss';
 
 const Mainsection = () => {
+
    gsap.registerPlugin(ScrollTrigger);
 
    const donutsBg = useRef(null);
 
    useEffect(
       () => {
-         const tl = gsap.timeline({ScrollTrigger: {trigger: '.mainSection', scrub: true, start: 'top top', end: 'bottom top' }});
-         tl.to(donutsBg.current, {y: '-=100'})
+
+         const tl = gsap.timeline({
+            scrollTrigger: {
+               trigger: '.welcome',
+               start: 'top top',
+               end: 'bottom center',
+               scrub: true
+            }
+         });
+
+         tl.to(donutsBg.current, {y: 120}, 0);
       },
       []
    )
